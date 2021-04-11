ad39089b1a531cde4b1cb74a41fb0797496df668
ry0y4n <rusk.ryoy4n@gmail.com>
2021-04-12 04:50:40 +0900

    fix: hoge

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,10 +8,19 @@ module.exports = {
   },
   dest: "public",
   head: [
-    [
-      //pwa 用のmanifest.json
-      ["link", { rel: "manifest", href: "/manifest.json" }],
-    ],
+    // [
+    //   //pwa 用のmanifest.json
+    //   ["link", { rel: "manifest", href: "/manifest.json" }],
+    // ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/sushi.JPG' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/sushi.JPG' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+
     [
       "link",
       {
