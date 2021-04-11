d861ba5f98b9bf9922738b175689c54d3e166073
ry0y4n <rusk.ryoy4n@gmail.com>
2021-04-11 21:47:52 +0900

    hoge

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,76 +1,82 @@
 module.exports = {
-  "title": "Momo Blog",
-  "description": "日々考えていることを落としておくためのサイト",
-  "dest": "public",
-  "head": [
+  title: "Momo Blog",
+  description: "日々考えていることを落としておくためのサイト",
+  locales: {
+    '/': {
+      lang: 'ja-JP',
+    },
+  },
+  dest: "public",
+  head: [
     [
       "link",
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
+        rel: "icon",
+        href: "/favicon.ico"
       }
     ],
     [
       "meta",
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no"
       }
     ],
     // ["script", { src: "temp.js" }]
   ],
-  "theme": "reco",
-  "themeConfig": {
-    "smoothScroll": "true",
-    "nav": [
+  theme: "reco",
+  themeConfig: {
+    lang: 'ja-JP',
+    smoothScroll: "true",
+    nav: [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
+        text: "Home",
+        link: "/",
+        icon: "reco-home"
       },
       {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: "TimeLine",
+        link: "/timeline/",
+        icon: "reco-date"
       },
       {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
+        text: "Contact",
+        icon: "reco-message",
+        items: [
           {
-            "text": "GitHub",
-            "link": "https://github.com/ry0y4n",
-            "icon": "reco-github"
+            text: "GitHub",
+            link: "https://github.com/ry0y4n",
+            icon: "reco-github"
           },
           {
-            "text": "twitter",
-            "link": "https://mobile.twitter.com/AnoTensai",
-            "icon": "reco-twitter"
+            text: "twitter",
+            link: "https://mobile.twitter.com/AnoTensai",
+            icon: "reco-twitter"
           }
         ]
       }
     ],
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "Category"
       },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
+      tag: {
+        location: 3,
+        text: "Tag"
       }
     },
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "sidebar": "auto",
-    "lastUpdated": "Last Updated",
-    "author": "momosuke",
-    "authorAvatar": "/sushi.JPG",
-    "startYear": "2019"
+    logo: "/logo.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    sidebar: "auto",
+    lastUpdated: "Last Updated",
+    author: "momosuke",
+    authorAvatar: "/sushi.JPG",
+    startYear: "2019"
   },
-  "markdown": {
-    "lineNumbers": true
+  markdown: {
+    lineNumbers: true
   }
 }
--- public/assets/js/29.969a41ee.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{236:function(n,w,o){}}]);
