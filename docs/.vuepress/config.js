5589f5b204ed5a8678daec4a228f2d94adc672e2
ry0y4n <rusk.ryoy4n@gmail.com>
2021-04-13 23:33:02 +0900

    hoge

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,18 +8,15 @@ module.exports = {
   },
   dest: "public",
   head: [
-    // [
-    //   //pwa 用のmanifest.json
-    //   ["link", { rel: "manifest", href: "/manifest.json" }],
-    // ],
+    ['link', { rel: 'icon', href: '/sushi.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/sushi.JPG' }],
+    ['link', { rel: 'apple-touch-icon', href: '/sushi-152.png' }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/sushi.JPG' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'msapplication-TileImage', content: '/sushi-144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 
     [
       "link",
@@ -99,5 +96,5 @@ module.exports = {
             buttonText: "Refresh"
         }
     }
-}
+  }
 }
